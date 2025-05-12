dd9e1ba478d675eebce5c0483f26c81c03ae7673
Ward Langeraert <ward.langeraert@inbo.be>
2025-05-12 10:07:48 +0200

    finish function

--- source/R/plot_richness.R
+++ source/R/plot_richness.R
@@ -1,6 +1,14 @@
-plot_richness <- function(data_cube, ref_grid) {
-  joined_sf <- dplyr::left_join(data_cube, ref_grid, by = "mgrscode") |>
+plot_richness <- function(richness_cube, ref_grid) {
+  require("ggplot2")
+
+  # Join richness and reference grid
+  joined_sf <- dplyr::left_join(richness_cube, ref_grid, by = "mgrscode") |>
     sf::st_sf(sf_column_name = "geom", crs = sf::st_crs(ref_grid))
   
+  # Visualise result
+  p <- ggplot(joined_sf) +
+    geom_sf(aes(fill = n_species)) +
+    theme_minimal()
   
+  return(p)
 }

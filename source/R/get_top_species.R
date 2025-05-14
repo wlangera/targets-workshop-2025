bcb092c2a6d77c27a82d22e70f03c79d4ac69afb
Ward Langeraert <ward.langeraert@inbo.be>
2025-05-14 10:22:19 +0200

    use base pipes

--- source/R/get_top_species.R
+++ source/R/get_top_species.R
@@ -3,8 +3,8 @@ get_top_species <- function(data_cube, top_n) {
 
   data_cube |>
     # Count number of grid cells per species
-    dplyr::count(species, name = "n_cells") %>%
+    dplyr::count(species, name = "n_cells") |>
     # Get top species
-    dplyr::slice_max(n_cells, n = top_n) %>%
+    dplyr::slice_max(n_cells, n = top_n) |>
     dplyr::pull(species)
 }
